162a28be9d67b3879a2a563f97ae9a2dfc41f910
Kafuluma Joachim <144176451+Joachim075@users.noreply.github.com>
2023-10-22 13:04:56 +0200

    Update index.js
    added delete, edit and checked button in  there funtions

--- index.js
+++ index.js
@@ -1,20 +1,59 @@
+const input = document.querySelector(".input-box");
+const ul = document.querySelector("ul");
+const btn = document.querySelector(".but");
 
-const body= document.querySelector("body");
-const input= document.querySelector(".input-box");
-const ol= document.querySelector("ol");
-const btn= document.querySelector(".but");
-btn.addEventListener("click", function() {
-    //creating the list itmem and its assign it to text
-    const liText=input.value;
-    const li=document.createElement("li");
-    li.innerText=liText;
-    ol.appendChild(li);
-    input.value="";
+// Load tasks from localStorage when the page loads
+const savedTasks = JSON.parse(localStorage.getItem('tasks')) || [];
 
-    //Saving to locastorage
-    let listArray=[];
-        listArray.push(liText);
-        localStorage.setItem('list', JSON.stringify(listArray))
+function addTask(taskText) {
+    const li = document.createElement("li");
+    li.innerHTML = `
+        <span class="task-text">${taskText}</span>
+        <span class="icons">
+            <i class="fas fa-check-circle complete"></i>
+            <i class="fas fa-edit edit"></i>
+            <i class="fas fa-trash-alt delete"></i>
+        </span>
+    `;
+    ul.appendChild(li);
+}
+
+// Load saved tasks when the page loads
+savedTasks.forEach(taskText => {
+    addTask(taskText);
 });
 
+btn.addEventListener("click", function() {
+    const taskText = input.value;
+    if (taskText.trim() !== "") {
+        addTask(taskText);
+        savedTasks.push(taskText);
+        localStorage.setItem('tasks', JSON.stringify(savedTasks));
+        input.value = "";
+    }
+});
 
+ul.addEventListener("click", function(event) {
+    const target = event.target;
+
+    if (target.classList.contains('complete')) {
+        const li = target.parentElement.parentElement;
+        li.classList.toggle("completed");
+    } else if (target.classList.contains('edit')) {
+        const li = target.parentElement.parentElement;
+        const text = li.querySelector('.task-text');
+        const newText = prompt("Edit task:", text.textContent);
+        if (newText !== null) {
+            text.textContent = newText;
+            const index = Array.from(ul.children).indexOf(li);
+            savedTasks[index] = newText;
+            localStorage.setItem('tasks', JSON.stringify(savedTasks));
+        }
+    } else if (target.classList.contains('delete')) {
+        const li = target.parentElement.parentElement;
+        const index = Array.from(ul.children).indexOf(li);
+        savedTasks.splice(index, 1);
+        localStorage.setItem('tasks', JSON.stringify(savedTasks));
+        ul.removeChild(li);
+    }
+});
